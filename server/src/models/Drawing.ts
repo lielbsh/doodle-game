b0ba5a049bf8f7a59ace52e795a9b94d595dd567
Assaf Assa <assafas141@gmail.com>
2025-02-12 18:52:47 +0200

    creatingfilenames

---
+++ server/src/models/Drawing.ts
@@ -0,0 +1,7 @@
+export interface Drawing {
+    id?: number;
+    player_id: string;
+    game_id: number;
+    created_at?: Date;
+  }
+  
---
+++ server/src/models/Score.ts
@@ -0,0 +1,7 @@
+export interface Score {
+    id?: number;
+    players: [string];
+    val: number;
+    created_at?: Date;
+  }
+  
