c86246fbcc875eb85e0b88a869542645299d574e
lielbsh <liel1939@gmail.com>
2025-02-27 22:07:21 +0200

    fix: correct sound path

--- client/src/utils/soundUtils.ts
+++ client/src/utils/soundUtils.ts
@@ -4,10 +4,10 @@ type SoundName = 'click' | 'timer' | 'endGame' | 'notification'| 'start'| 'slidi
 export const sounds: Record<SoundName, HTMLAudioElement> = {
   click: new Audio(`${process.env.PUBLIC_URL}/button-clicking-1.wav`),
   timer: new Audio(`${process.env.PUBLIC_URL}/Ticking Digital Clock.mp3`),
-  notification: new Audio(`${process.env.PUBLIC_URL}mixkit-quick-win-video-game-notification-269.wav`),
-  endGame : new Audio(`${process.env.PUBLIC_URL}mixkit-instant-win-2021.wav`),
-  start: new Audio(`${process.env.PUBLIC_URL}mixkit-unlock-game-notification-253.wav`),
-  sliding: new Audio(`${process.env.PUBLIC_URL}mixkit-sliding-wooden-light-door-1522.wav`)
+  notification: new Audio(`${process.env.PUBLIC_URL}/mixkit-quick-win-video-game-notification-269.wav`),
+  endGame : new Audio(`${process.env.PUBLIC_URL}/mixkit-instant-win-2021.wav`),
+  start: new Audio(`${process.env.PUBLIC_URL}/mixkit-unlock-game-notification-253.wav`),
+  sliding: new Audio(`${process.env.PUBLIC_URL}/mixkit-sliding-wooden-light-door-1522.wav`)
 };
 
 export const playSound = (soundName: SoundName): void => {
