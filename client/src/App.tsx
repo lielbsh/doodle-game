695e7267641b9b4fb38656919aafd2e5d3253fc2
lielbsh <liel1939@gmail.com>
2025-02-27 20:53:50 +0200

    fix: No routes matched location error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import GamePage from "./components/GamePage";
 
