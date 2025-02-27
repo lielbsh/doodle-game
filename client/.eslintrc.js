64e7244ef48d0b111e2919d20550ab66bac5d68d
lielbsh <liel1939@gmail.com>
2025-02-27 13:54:49 +0200

    fixing error eslint

--- client/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: ['react-app', 'react-app/jest'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    rules: {
-      // Add any custom rules you need
-    },
-  };
-  
